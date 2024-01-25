c35ce48655c98a7a6b7bfdc276e356da040e6366
pgen0x <protogenox@gmail.com>
2024-01-25 23:40:30 +0700

    style: add new scrable text on button view details and buy now

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-import Image from "next/image";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 import Nft from "@/components/Nft";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 async function getNFTs({ pageParam }: { pageParam: number }) {
   const res = await fetch(
-    `https://qrc721api.amnst.tech/api/nfts?limit=20&page=${pageParam}`
+    `https://qrc721api.amnst.tech/api/nfts?limit=4&page=${pageParam}`
   );
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
@@ -35,7 +34,7 @@ export default function Home() {
     queryFn: getNFTs,
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
-      const totalPages = lastPage.totalPages;
+      const totalPages = lastPage.totalPages; // Assuming totalPages is present in the API response
       const currentPage = lastPage.currentPage;
 
       // Check if there are more pages to fetch
@@ -52,12 +51,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="mt-4 text-3xl">Welcome to Quai NFT Marketplace!</h2>
-      <div className="w-full md:w-10/12 m-auto flex mt-5 mb-5 flex-col md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:grid-row-1 md:items-center gap-4">
+      <div className="w-full md:w-10/12 m-auto flex mt-5 mb-5 flex-col md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 md:grid-row-1 md:items-center gap-4">
         {nfts?.pages?.map((page, pageIndex) =>
           page.nfts.map((nft: any, index: number) => (
             <Nft
               image={nft.imageUri}
               name={nft.name}
+              tokenId={nft.tokenId}
               key={pageIndex * 20 + index}
               innerRef={ref}
             />
--- components/Nft.tsx
+++ components/Nft.tsx
@@ -1,28 +1,95 @@
-import React from "react";
 import Image from "next/image";
+import React, { useState } from "react";
+import { ScrambledAnimation } from "react-scrambled-text";
 
 interface NftProps {
   name: string;
   image: string;
+  tokenId: string;
   innerRef?: (node?: Element | null | undefined) => void;
 }
 
-const Nft: React.FC<NftProps> = ({ name, image, innerRef }) => {
+interface ScrambleButtonProps {
+  texts: string[];
+  speed: number;
+  pauseDuration: number;
+}
+
+const ScrambleButton: React.FC<ScrambleButtonProps> = ({
+  texts,
+  speed,
+  pauseDuration,
+}) => {
+  const [pause, setPause] = useState(false);
+
+  const handleMouseEnter = () => {
+    setPause(true);
+
+    setTimeout(() => {
+      setPause(false);
+    }, pauseDuration);
+  };
+
+  const handleMouseLeave = () => {
+    setPause(false);
+  };
+
+  return (
+    <button
+      className="button text-white inline-flex justify-center items-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <ScrambledAnimation
+        texts={texts}
+        speed={speed}
+        pauseDuration={pauseDuration}
+        start={pause}
+        style={{ color: "white", fontSize: "12px" }}
+      />
+    </button>
+  );
+};
+
+const Nft: React.FC<NftProps> = ({ name, image, tokenId, innerRef }) => {
+  const [pause, setPause] = useState(false);
+
+  const handleMouseEnter = () => {
+    setPause(true);
+
+    // Automatically set pause back to false after 500ms
+    setTimeout(() => {
+      setPause(false);
+    }, 500);
+  };
   return (
     <React.Fragment>
       <div ref={innerRef}>
-        <div className="flex flex-col cursor-pointer shadow-lg border-2 border-gray-500/30 border-dotted gap-4 items-center p-2 rounded-2xl">
-          <div className="rounded-full m-4">
+        <div className="flex flex-col shadow-lg border-[1px] border-white gap-4 items-center p-4 rounded-2xl">
+          <div className="rounded-full ">
             <Image
               src={image}
               alt="profileImg"
-              width={256}
-              height={256}
-              className="z-10 w-56 h-56 rounded-2xl bg-white object-cover"
+              width={176}
+              height={176}
+              className="w-44 h-44 rounded-2xl bg-white object-cover"
             />
           </div>
           <div>
-            <span className="text-white">{name}</span>
+            <span className="text-white">{name}</span>{" "}
+            <span className="text-white">#{tokenId}</span>
+          </div>
+          <div className="flex gap-4 justify-between items-center">
+            <ScrambleButton
+              texts={["View Detail", "View Detail"]}
+              speed={25}
+              pauseDuration={500}
+            />
+            <ScrambleButton
+              texts={["Buy Now", "Buy Now"]}
+              speed={25}
+              pauseDuration={500}
+            />
           </div>
         </div>
       </div>
